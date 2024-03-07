9cefba982bf2456b8b7014d87dc2e9e01674ffad
anmolbansal7 <abanmolbansal5@gmail.com>
2024-03-07 19:24:15 +0530

    video bg

--- app/home/index.js
+++ app/home/index.js
@@ -9,12 +9,23 @@ import ImageFooter from "@/core/components/ImageFooter";
 const Home = () => {
 	return (
 		<main>
-			<div className={styles.landing}>
+			<div className={styles.video_container}>
+				<div className={styles.mouse} />
+				<video
+					src={require("/public/videos/video.mp4")}
+					autoPlay
+					muted
+					loop
+					className={styles.video}
+				/>
+			</div>
+
+			{/* <div className={styles.landing}>
 				<div className={styles.content}>
 					Where Dreams Unfold <br /> and Memories Begin
 				</div>
 				<div className={styles.mouse} />
-			</div>
+			</div> */}
 			<Info />
 			<About />
 			<Projects />
