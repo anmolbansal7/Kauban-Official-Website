f79fbf3c740cd7e813c2419a3eaafab16a8ee68a
anmolbansal7 <abanmolbansal5@gmail.com>
2024-03-07 19:36:43 +0530

    deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 import withVideos from "next-videos";
-const nextConfig = { ...withVideos() };
+const nextConfig = {
+	basePath: "/kauban-website",
+	output: "export",
+	reactStrictMode: true,
+	...withVideos(),
+};
 
 export default nextConfig;
